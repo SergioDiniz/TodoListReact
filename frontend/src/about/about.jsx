f41f64984efe27e22579e72371170ac1a7259a0e
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-07 14:14:19 -0300

    seprando componentes de form e list

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 const about = (props) => {
     return(
         <div>
-            <h1>Sobre</h1>
+            <PageHeader name='Sobre' small='Nós'/>
+
+            <h2>Nossa História</h2>
+            <p>
+                Aenean placerat. In vulputate urna eu arcu. Aliquam erat volutpat. 
+                Suspendisse potenti. Morbi mattis felis at nunc. 
+                Duis viverra diam non justo. In nisl. 
+                Nullam sit amet magna in magna gravida vehicula. 
+                Mauris tincidunt sem sed arcu. Nunc posuere. 
+                Nullam lectus justo,
+            </p>
+
+            <h2>Missão e Visão</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi gravida libero nec velit.
+                 Morbi scelerisque luctus velit. Etiam dui sem, fermentum vitae, sagittis id, 
+                 malesuada in, quam. Proin mattis lacinia justo. Vestibulum facilisis auctor 
+                 urna. Aliquam in lorem sit amet leo accumsan lacinia. Integer rutrum, orci 
+                 vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat 
+                 pede sem sit amet enim. Phasellus et lorem id felis nonummy placerat. 
+                 Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean 
+                 vel massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque 
+                 libero. Maecenas libero. Etiam dictum tincidunt diam. Donec ipsum massa,
+                  ullamcorper in, auctor et, scelerisque sed, est. Suspendisse nisl. Sed convallis 
+                  magna eu sem. Cras pede libero, dapibus nec, pretium sit amet, tempor quis, 
+                  urna. Maecenas ipsum velit, consectetuer eu, lobortis ut, dictum at, dui. In 
+                  rutrum. Sed ac dolor sit amet purus malesuada congue. In laoreet, magna id 
+                  viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. 
+                  Suspendisse sagittis ultrices augue. Mauris metus. Nunc dapibus tortor vel mi 
+                  dapibus sollicitudin. Etiam posuere lacus quis dolor.
+            </p>
+
+            <h2>Impresa</h2>
+            <p>
+                Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, 
+                ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis 
+                velit mauris vel metus. Aenean fermentum risus id tortor. Integer 
+                imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo 
+                pretium faucibus. Mauris elementum mauris vitae tortor. In dapibus 
+                augue non sapien. Aliquam ante. Curabitur bibendum justo non orci.
+            </p>
         </div>
     )
 }
