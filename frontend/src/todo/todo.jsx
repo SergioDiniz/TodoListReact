79c4a7a9967299f78e01b19878d80e6b57ea0a8c
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-07 16:35:02 -0300

    cadastrando tarefa

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 class Todo extends Component {
 
@@ -20,7 +23,11 @@ class Todo extends Component {
     }
 
     handleAdd(){
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => {
+                console.log('cadastrou!')
+            })
     }
 
     render() {
