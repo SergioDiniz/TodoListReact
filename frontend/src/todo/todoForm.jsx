13d083b201516fec6ff0db69706ff00d8e8c16ea
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-07 16:18:59 -0300

    getting form value

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,7 +6,8 @@ const TodoForm = (props) => {
     return(
         <div className='todoForm'>
             <div className="col-xs-12 col-sm-8 col-md-10">
-                <input className='form-control input-lg' id='description' type='text' placeholder='Adicione uma tarefa' ></input>
+                <input onChange={props.handleChange}
+                    className='form-control input-lg' id='description' type='text' placeholder='Adicione uma tarefa' ></input>
             </div>
 
             <div className='col-xs-12 col-sm-4 col-md-2'>
