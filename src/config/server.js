332dd931ba0df7ad0a2d20b6f339afa9fc80da16
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-07 10:12:48 -0300

    cors

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,11 @@ const port = 3003
 const bodyParse = require('body-parser')
 const express = require('express')
 const server = express()
+const allorCors = require('./cors')
 
 server.use(bodyParse.urlencoded({extended: true}))
 server.use(bodyParse.json())
+server.use(allorCors)
 
 
 server.listen(port, function(){
