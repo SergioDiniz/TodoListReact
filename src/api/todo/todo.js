ab3f837c336c2eca1a31b8f8aeaa3149745950f9
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-07 09:58:02 -0300

    rotas

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 
 const todoScheme = new mongoose.Schema({
     description: {type: String, required: true},
-    done: {Type: Boolean, required: true, default: false},
+    done: {type: Boolean, required: true, default: false},
     createdAt: {type: Date, default: Date.now}
 })
 
-module.exports = restful.module('todo'. todoScheme)
+module.exports = restful.model('todo', todoScheme)
